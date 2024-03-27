83fea9bc69e88a28756a51b6c20be623bb6f3e60
Hiroki Ando <hikuohiku@gmail.com>
2024-03-27 23:28:22 +0900

    commit

--- fish/conf.d/alias.fish
+++ fish/conf.d/alias.fish
@@ -2,14 +2,14 @@ alias pbcopy='xclip -selection c'
 alias pbpaste='xclip -selection c -o'
 alias ls='ls -F --color=auto'
 alias l='ls -lah'
-abbr --add c 'code'
-abbr --add v 'nvim'
-abbr --add vim 'nvim'
-abbr --add zl 'zellij'
+abbr --add c code
+abbr --add v nvim
+abbr --add vim nvim
+abbr --add zl zellij
 abbr --add ll 'ls -l'
 abbr --add la 'ls -A'
 abbr --add lla 'ls -l -A'
-abbr --add g 'git'
+abbr --add g git
 abbr --add gst 'git status'
 abbr --add gsw 'git switch'
 abbr --add gbr 'git branch'
@@ -19,8 +19,8 @@ abbr --add gad 'git add'
 abbr --add gcm 'git commit'
 abbr --add gmg 'git merge'
 abbr --add gpsh 'git push'
-abbr --add lg 'lazygit'
+abbr --add lg lazygit
 
 function code
-  command code $argv --ozone-platform-hint=wayland --enable-wayland-ime
+    command code $argv --ozone-platform-hint=wayland --enable-wayland-ime
 end
